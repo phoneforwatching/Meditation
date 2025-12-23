e8094120078a8045ad8ec80a83a6a0ad512a475f
iphone <iphone@example.com>
2025-12-24 01:23:37 +0700

    feat: Add basic SSL plugin and enable HTTPS for local development.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		basicSsl()
+	],
 	server: {
 		port: 5174,
+		https: true,
 		allowedHosts: ['erna-colourational-suffusedly.ngrok-free.dev']
 	},
 	build: {
